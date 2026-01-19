bf53795d73db2dc84543a9451bb36cdd9b1dc955
orion <nnmm5122004@gmail.com>
2026-01-19 22:26:56 +0630

    :tada: init commit

---
+++ src/main/java/com/dev/japanese_app/JapaneseAppApplication.java
@@ -0,0 +1,13 @@
+package com.dev.japanese_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaneseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapaneseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/japanese_app/JapaneseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.japanese_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapaneseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
