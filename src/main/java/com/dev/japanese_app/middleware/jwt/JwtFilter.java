67accaa6fe9768931e93e9a15ab7a8c323027465
Orion <nnmm5122004@gmail.com>
2026-01-27 08:46:58 +0630

    :sparkles: add whitelist for jwtfilter

--- src/main/java/com/dev/japanese_app/middleware/jwt/JwtFilter.java
+++ src/main/java/com/dev/japanese_app/middleware/jwt/JwtFilter.java
@@ -1,5 +1,6 @@
-package com.dev.japanese_app.security.jwt;
+package com.dev.japanese_app.middleware.jwt;
 
+import com.dev.japanese_app.common.constant.SecurityConstants;
 import com.dev.japanese_app.features.user.repo.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -8,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -18,9 +20,15 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter {
 
     private final UserRepository userRepository;
+    private AntPathMatcher antPathMatcher = new AntPathMatcher();
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        for (String urlPattern : SecurityConstants.WHITE_LISTS_URL) {
+            if (antPathMatcher.match(urlPattern, request.getRequestURI())){
+                filterChain.doFilter(request,response);
+            }
+        }
     }
 }
